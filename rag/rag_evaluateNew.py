0aa0dfcafa7950ba4fcfba14dcc82d2c624444ac
성우현 <sunguh0904@gmail.com>
2024-10-02 12:44:59 +0900

    임시 저장

--- rag/rag_evaluateNew.py
+++ rag/rag_evaluateNew.py
@@ -95,13 +95,13 @@ def searchDocs_evaluate(answers: str, index_name: str, type: str, explain=True, 
     )
 
     hits = response['hits']['hits']
-    
+
     print("\n유사성 판단 근거:")
     for i, hit in enumerate(hits):
         print(f"\n문서 {i+1}:")
         print(f"질문: {hit['_source']['question']}")
         print(f"유사도 점수: {hit['_score']:.2f}")
-    
+
         if '_explanation' in hit:
             explanation = hit['_explanation']
             print("유사성 판단 이유:")
@@ -120,7 +120,7 @@ def print_human_readable_explanation(explanation):
             print(f"- 총 유사도 점수: {explanation['value']:.2f}")
         elif 'product of' in desc:
             print(f"- 최종 유사도 점수: {explanation['value']:.2f}")
-    
+
     if 'details' in explanation:
         for detail in explanation['details']:
             print_human_readable_explanation(detail)
@@ -150,12 +150,10 @@ def evaluate_questions(question, answer, years, job, type, combined_context, num
         - Score strictly according to the 'Scoring Scale' above only.
         - For an 'A' score, the answer must correctly include the concept of the technology mentioned in the question, plus any additional correct information related to that technology. Always give an 'A' score if there's any correct information beyond the basic concept, regardless of its depth or amount.
         - Only assign scores based on correct information. If any part of the answer is incorrect, adjust the score accordingly.
-        - Do not consider the depth, specificity, or amount of additional information. Any correct additional information beyond the basic concept is sufficient for an 'A' score.
-        - Do not arbitrarily consider other elements when scoring, such as specific details, examples, or in-depth explanations.
         - Do not include any contents related to 'Scoring Scale' or score in the explanation.
         - Provide a model answer to the question, considering the interviewee's role and experience. This model answer should demonstrate the correct concept and include some additional correct information.
         - The model answer must consist only of content that can be verbally expressed. Do not include special characters such as hyphens or colons.
-        - Evaluate the answer on a scale of 1 to 100 based on the following criteria: problem-solving, technical understanding, logical thinking, learning ability, and collaboration/communication.
+        - Evaluate the answer based on the following five criteria: problem-solving, technical understanding, logical thinking, learning ability, and collaboration/communication. Assign a score between 1 and 100 for each criterion.
         - If a criterion is not present in the answer, assign a null value, and only assign a score if the criterion is included.
 
         # Policy
@@ -186,42 +184,78 @@ def evaluate_questions(question, answer, years, job, type, combined_context, num
     elif type == "behavioral":
         prompt = f"""
         # Role
-        - You are a personality interviewer.
-        
-        # Context
-        - The interviewee is in the {job} position and is undergoing a personality interview at a related company.
-        
+        You are a character interviewer with expertise in conducting interviews.
+
         # Task
-        - Make sure the interviewee's {answer} includes at least one of the following in relation to the company: self-motivation, self-awareness, honesty, adaptability, and interpersonal skills.
-        - Evaluate the given answer {answer} to the question {question} using the following grading scale:
-            A: Excellent
-            B: Good
-            C: Satisfactory
-            D: Poor
-            E: Unsatisfactory or No relevant content
-            F: No answer or no honesty/trustworthiness, interpersonal, self-awareness, or adaptability content.
+        Evaluate the answer based on the following criteria:
+        - Interviewer's job: {job}
+        - Interviewer's experience level: {years} years
+        - Interviewer's answer: {answer}
+        - Question: {question}
         
+        ### A-grade Example:
+        The incident involving the district chief being acquitted despite charges of inadequate response highlights the importance of accountability in public safety. Public institutions are entrusted with ensuring citizen safety, which involves proactive measures, transparent communication, and clearly defined responsibilities.
+        This perspective aligns closely with my values as an AI engineer. Just as public institutions must act transparently and responsibly, I believe that my role as an engineer requires the same standards. When developing AI systems, especially those with safety implications, it’s crucial to ensure transparency in decision-making processes, rigorous testing, and accountability for outcomes. For instance, AI models that predict crowd dynamics during events can help enhance public safety by providing timely alerts.
+        Working at this company, I aim to contribute by applying these principles—developing AI technology that not only meets technical standards but also serves a social good, ensuring trust and reliability in our solutions.
+        
+        ### B-grade Example:
+        The responsibility of public institutions is vital for citizen safety, and recent incidents have shown that proactive responses and transparency are essential. Although there may not have been legal liability in this case, public institutions still need to demonstrate ethical accountability.
+        Similarly, as an AI engineer, I believe my role requires me to approach development responsibly. By predicting potential risks and ensuring transparency in our AI models, I can contribute to public safety.
+        
+        ### C-grade Example:
+        Public institutions must protect citizens and ensure their safety. Even if no legal wrongdoing is found, the responsibility for public safety should be taken seriously, and improvements should be made when needed.
+        As an engineer at this company, I aim to use AI to predict risks and contribute to enhancing public safety.
+        
+        ### D-grade Example:
+        The responsibility of public institutions is important, and their role is to protect citizens. I think better responses could have been made in the incident mentioned.
+        At this company, I hope to help improve public safety through technology.
+        
+        ### E-grade Example:
+        Public institutions need to be responsible. The issue seems not to have been handled well.
+        I want to contribute by working diligently at the company.
+        
+        ### F-grade Example:
+        I'm not sure about public institutions, but I would like to work at this company.
+
+        # Scoring Scale
+        A: The answers are specific, coherent, and logically well-organized, perfectly reflecting the key personality elements of the question. In addition to the main elements, other personality elements are also well reflected, showing the depth and insight of the answers.
+        B: Your answers faithfully reflect key elements of your personality and provide logical and relevant explanations. However, it may lack specificity or sufficient reference to other personality factors.
+        C: Understand some of the key personality factors in the question and provide relevant basic answers. However, the answers may be somewhat general, lack specificity, or reflect little beyond the main elements.
+        D: There is a lack of understanding of some of the key personality elements of the question, or the answer is not logical. Although the main elements are mentioned very briefly, there is little depth or concrete examples, and there is no reflection of other personality elements.
+        E: The answers are answered in a way that does not match the intent of the question, rarely reflect relevant aspects of key personality factors, or are very superficial in content.
+        F: Included when there is no answer, when you do not know the question, or when the answer is unrelated to the question.
+
+        # Instructions
+        - Score strictly according to the 'Scoring Scale' above only.
+        - You should base your score on accurate information, and if any part of your answer is incorrect, your score should be adjusted accordingly.
+        - Do not arbitrarily consider other elements when scoring, such as specific details, examples, or in-depth explanations.
+        - Don't include anything related to your score in your description.
+        - Provide a model answer to the question, considering the interviewee's role and experience. This model answer should demonstrate the correct concept and include some additional correct information.
+        - Model answers should consist only of what can be expressed orally.
+        - Responses are evaluated on a scale of 1 to 100 based on criteria such as “honesty (trust),” “interpersonal relationships,” “adaptability,” “self-motivation (passion),” and “self-awareness.”
+        - If the answer contains no criteria, we assign a null value to the "score" value and assign a score only if the answer contains the criteria.
+        - The scores for each element in "criteria_scores" must be very strict.
+        - If a criterion can be evaluated, assign a score; if it cannot be evaluated, assign a NULL value.
+        - Determine the core elements or key personality factors that the question is intended to evaluate before scoring the response.
+
         # Policy
-        - The description must not mention scores.
-        - The answer to the question reflects the reasons explained, taking into account the interviewee's job {job} and experience, and presents an ideal answer.
-        - The ideal answer should consist only of things that can be expressed verbally.
-        - Strictly evaluate the answer based on the following five criteria: honesty_reliability, interpersonal skills, self motivation passion, adaptability, and self awareness. Assign a score between 1 and 100 for each criterion.
-        - If a criterion is not present in the answer, assign a null value, and only assign a score if the criterion is included.
-        - Refer to users as '면접자'.
-        - The evaluation must be detailed and include at least one of the following elements: honesty/trustworthiness, self-awareness, interpersonal relationships, self-motivation, and adaptability.
-        - Only the personality aspect is evaluated in the answer. Technical aspects should not be considered.
-        - The score must be evaluated strictly according to the 'Scoring Scale' and expressed as an alphabetical letter.
+        - Responses must be provided in Korean only.
+        - Technical aspects should never be considered in the interviewer's answers, only personality aspects should be considered.
+        - You must create an explanation for the correct answer, focusing only on personality content.
+        - A model answer is created by reflecting the explanation.
+        - The 'score' value must be expressed as an alphabetical letter.
         - Responses must be in JSON format.
         - Place the score in the `score` value of the JSON output.
         - Place the explanation in the `explanation` value of the JSON output.
-        - Place the intention of the question in the `intention` value of the JSON output.
-        - Do not include any additional explanations beyond the specified output format.
-
+        - Place the model answer in the `model` value of the JSON output.
+        - It must not contain any additional description beyond the specified output format.
+        - You must evaluate whether each element of "criteria_scores" has been answered correctly as the question was intended.
+        
         # Output Format
         {{
             "score": "",
             "explanation": "",
-            "intention": "",
+            "model": "",
             "criteria_scores": {{
                 "honesty_reliability": null,
                 "interpersonal_skills": null,
